276d757d2e2c76d371c35a92541390118faddc41
sakai-yu <yu.sakai@hpk.co.jp>
2025-12-11 08:29:15 +0900

    update:assets_dirで現在のディレクトリを指定するように変更

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ def main(page: ft.Page):
     page.title = "My Flet App"
     page.add(
         ft.Text("Hello, Flet on Web!"),
-        ft.Image(src="70291.jpg",width=300,height=300)
-        )
+        ft.Image(src="70291.jpg", width=300, height=300)
+    )
 
 if __name__ == "__main__":
-    # Render や Railway では PORT 環境変数にポート番号が入ることが多い
     port = int(os.environ.get("PORT", 8080))
-
     ft.app(
     target=main,
     view=ft.AppView.WEB_BROWSER,
-    port=int(os.environ.get("PORT", 8080))
+    host="0.0.0.0",
+    port=port,
+    assets_dir="."
 )
